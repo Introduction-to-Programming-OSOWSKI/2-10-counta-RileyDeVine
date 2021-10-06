d22cd5fe35fbdf87b3e062871ae0fe9e7f065716
rileydevine <824055@mystma.org>
2021-10-06 09:18:12 -0500

    submitted

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #WRITE YOUR CODE IN THIS FILE
+
+#define function
+def countA(w):
+    total = 0
+    for i in range(0,len(w)):
+        if w[i] == "a":
+            total = total + 1
+    return total
+
+print(countA("baseball"))
+
